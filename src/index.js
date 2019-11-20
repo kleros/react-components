3350ff07d7b3ef4b2e95dee68bd3eb934bee022d
Ferit Tunçer <ferit@cryptolab.net>
2019-11-20 18:54:47 +0300

    refactor(footer): add etherscan github and slack and use inline svg

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./module";
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App style={{ fontSize: "14px" }} />,
+  document.getElementById("root")
+);
