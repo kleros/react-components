015f301b0b8bd973728be1d4eb87790319988732
Ferit Tunçer <ferit@cryptolab.net>
2020-01-22 14:59:14 +0300

    refactor(debug): export evidence display (unused)

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,4 +4,5 @@ import "./index.css";
 export { default as Footer } from "./footer.js";
 export { default as EvidenceTimeline } from "./evidence-timeline.js";
 export { default as EvidenceCard } from "./evidence-card.js";
+export { default as EvidenceDisplay } from "./evidence-display.js";
 export { default as Attachment } from "./attachment.js";
