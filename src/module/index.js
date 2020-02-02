38123d7354d9e82ef1074b5417e065bfc56f8184
Ferit Tunçer <ferit@cryptolab.net>
2020-02-02 19:56:11 +0300

    refactor(evidence-timeline): styling improvements

--- src/module/index.js
+++ src/module/index.js
@@ -1,78 +1,68 @@
 import React from "react";
 import { Footer } from "../lib/index";
 import { EvidenceTimeline } from "../lib/index";
+import Archon from "@kleros/archon";
 
-const App = () => (
-  <>
-    <EvidenceTimeline
-      evidences={[
-        {
-          evidenceJSON: {
-            fileURI:
-              "QmbY6fRVgXXnNcmhoYRb4JCPU8y9uKmdWVFLoj5fBRb1av/evidence-zb-token-.mov",
-            fileHash:
-              "Bce1WTQa7bfrJMFdEJuWV2xHsmj5JcDDyqBKGXu6PHZsn5e5oxkJ8cMJcuFDK1VsQYBtfrzgWkKCovWSvsacgN1XTj",
-            name: "Example Evidence",
-            description:
-              "This evidence shows how to properly utilize hashing and Archon to submit valid evidence!"
-          },
-          evidenceValid: true,
-          fileValid: true,
-          submittedBy: "0xac96570CF5bb71e557f117651Ce4B100B6d06af8",
-          submittedAt: 1579543075
-        },
-        {
-          evidenceJSON: {
-            fileURI:
-              "QmbY6fRVgXXnNcmhoYRb4JCPU8y9uKmdWVFLoj5fBRb1av/evidence-zb-token-.png",
-            fileHash:
-              "Bce1WTQa7bfrJMFdEJuWV2xHsmj5JcDDyqBKGXu6PHZsn5e5oxkJ8cMJcuFDK1VsQYBtfrzgWkKCovWSvsacgN1XTj",
-            name: "Example Evidence",
-            description:
-              "This evidence shows how to properly utilize hashing and Archon to submit valid evidence!"
-          },
-          evidenceValid: true,
-          fileValid: true,
-          submittedBy: "0xac96570CF5bb71e557f117651Ce4B100B6d06af8",
-          submittedAt: 1579543075
-        },
-        {
-          evidenceJSON: {
-            fileURI:
-              "QmbY6fRVgXXnNcmhoYRb4JCPU8y9uKmdWVFLoj5fBRb1av/evidence-zb-token-.txt",
-            fileHash:
-              "Bce1WTQa7bfrJMFdEJuWV2xHsmj5JcDDyqBKGXu6PHZsn5e5oxkJ8cMJcuFDK1VsQYBtfrzgWkKCovWSvsacgN1XTj",
-            name: "Example Evidence",
-            description:
-              "This evidence shows how to properly utilize hashing and Archon to submit valid evidence!"
-          },
-          evidenceValid: true,
-          fileValid: true,
-          submittedBy: "0xac96570CF5bb71e557f117651Ce4B100B6d06af8",
-          submittedAt: 1579543075
-        },
-        {
-          evidenceJSON: {
-            fileURI:
-              "QmbY6fRVgXXnNcmhoYRb4JCPU8y9uKmdWVFLoj5fBRb1av/evidence-zb-token-.a",
-            fileHash:
-              "Bce1WTQa7bfrJMFdEJuWV2xHsmj5JcDDyqBKGXu6PHZsn5e5oxkJ8cMJcuFDK1VsQYBtfrzgWkKCovWSvsacgN1XTj",
-            name: "Example Evidence",
-            description:
-              "This evidence shows how to properly utilize hashing and Archon to submit valid evidence!"
-          },
-          evidenceValid: true,
-          fileValid: true,
-          submittedBy: "0xac96570CF5bb71e557f117651Ce4B100B6d06af8",
-          submittedAt: 1579543075
-        }
-      ]}
-    />
-    <br />
-    <Footer
-      appName="Test Application"
-      repository="https://github.com/kleros/react-components"
-    />
-  </>
-);
+const archon = new Archon(window.ethereum, "https://ipfs.kleros.io");
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { metaevidence: "", evidences: [] };
+  }
+
+  getMetaEvidence = async () => {
+    const disputeLog = await archon.arbitrable.getDispute(
+      "0xebcf3bca271b26ae4b162ba560e243055af0e679", // arbitrable contract address
+      "0x988b3A538b618C7A603e1c11Ab82Cd16dbE28069", // arbitrator contract address
+      132 // dispute unique identifier
+    );
+
+    const metaevidence = await archon.arbitrable.getMetaEvidence(
+      "0xebcf3bca271b26ae4b162ba560e243055af0e679", // arbitrable contract address
+      disputeLog.metaEvidenceID
+    );
+
+    return metaevidence;
+  };
+
+  getEvidence = async () => {
+    const disputeLog = await archon.arbitrable.getDispute(
+      "0xebcf3bca271b26ae4b162ba560e243055af0e679", // arbitrable contract address
+      "0x988b3A538b618C7A603e1c11Ab82Cd16dbE28069", // arbitrator contract address
+      132 // dispute unique identifier
+    );
+
+    const evidence = await archon.arbitrable.getEvidence(
+      "0xebcf3bca271b26ae4b162ba560e243055af0e679",
+      "0x988b3A538b618C7A603e1c11Ab82Cd16dbE28069",
+      disputeLog.evidenceGroupID
+    );
+    console.log(evidence);
+    return evidence;
+  };
+
+  componentDidMount = async () => {
+    const metaevidence = await this.getMetaEvidence();
+    const evidences = await this.getEvidence();
+    this.setState({ metaevidence, evidences });
+  };
+
+  render() {
+    return (
+      <>
+        <EvidenceTimeline
+          metaevidence={this.state.metaevidence}
+          evidences={this.state.evidences}
+        />
+        <br />
+        <Footer
+          appName="Test Application"
+          repository="https://github.com/kleros/react-components"
+        />
+      </>
+    );
+  }
+}
+
 export default App;
