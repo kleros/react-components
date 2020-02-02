fc2183308c45b76f6a5e766befa1d423d4f5823d
Ferit Tunçer <ferit@cryptolab.net>
2020-02-02 18:18:38 +0300

    feat(evidence-timeline): variable attachment icons

--- src/module/index.js
+++ src/module/index.js
@@ -9,7 +9,52 @@ const App = () => (
         {
           evidenceJSON: {
             fileURI:
-              "QmbY6fRVgXXnNcmhoYRb4JCPU8y9uKmdWVFLoj5fBRb1av/evidence-zb-token-.pdf",
+              "QmbY6fRVgXXnNcmhoYRb4JCPU8y9uKmdWVFLoj5fBRb1av/evidence-zb-token-.mov",
+            fileHash:
+              "Bce1WTQa7bfrJMFdEJuWV2xHsmj5JcDDyqBKGXu6PHZsn5e5oxkJ8cMJcuFDK1VsQYBtfrzgWkKCovWSvsacgN1XTj",
+            name: "Example Evidence",
+            description:
+              "This evidence shows how to properly utilize hashing and Archon to submit valid evidence!"
+          },
+          evidenceValid: true,
+          fileValid: true,
+          submittedBy: "0xac96570CF5bb71e557f117651Ce4B100B6d06af8",
+          submittedAt: 1579543075
+        },
+        {
+          evidenceJSON: {
+            fileURI:
+              "QmbY6fRVgXXnNcmhoYRb4JCPU8y9uKmdWVFLoj5fBRb1av/evidence-zb-token-.png",
+            fileHash:
+              "Bce1WTQa7bfrJMFdEJuWV2xHsmj5JcDDyqBKGXu6PHZsn5e5oxkJ8cMJcuFDK1VsQYBtfrzgWkKCovWSvsacgN1XTj",
+            name: "Example Evidence",
+            description:
+              "This evidence shows how to properly utilize hashing and Archon to submit valid evidence!"
+          },
+          evidenceValid: true,
+          fileValid: true,
+          submittedBy: "0xac96570CF5bb71e557f117651Ce4B100B6d06af8",
+          submittedAt: 1579543075
+        },
+        {
+          evidenceJSON: {
+            fileURI:
+              "QmbY6fRVgXXnNcmhoYRb4JCPU8y9uKmdWVFLoj5fBRb1av/evidence-zb-token-.txt",
+            fileHash:
+              "Bce1WTQa7bfrJMFdEJuWV2xHsmj5JcDDyqBKGXu6PHZsn5e5oxkJ8cMJcuFDK1VsQYBtfrzgWkKCovWSvsacgN1XTj",
+            name: "Example Evidence",
+            description:
+              "This evidence shows how to properly utilize hashing and Archon to submit valid evidence!"
+          },
+          evidenceValid: true,
+          fileValid: true,
+          submittedBy: "0xac96570CF5bb71e557f117651Ce4B100B6d06af8",
+          submittedAt: 1579543075
+        },
+        {
+          evidenceJSON: {
+            fileURI:
+              "QmbY6fRVgXXnNcmhoYRb4JCPU8y9uKmdWVFLoj5fBRb1av/evidence-zb-token-.a",
             fileHash:
               "Bce1WTQa7bfrJMFdEJuWV2xHsmj5JcDDyqBKGXu6PHZsn5e5oxkJ8cMJcuFDK1VsQYBtfrzgWkKCovWSvsacgN1XTj",
             name: "Example Evidence",
